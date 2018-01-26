107a1774807fc89ed4c779e65cfee64e3e651309
Ryan Kennedy <33361057+orwells-ghost@users.noreply.github.com>
2018-01-25 22:45:55 -0800

    Updated gitignore and generate_map

--- generate_map.py
+++ generate_map.py
@@ -3,9 +3,15 @@ import folium
 # GPS Coords at Pioneer Courthouse
 map = folium.Map(location=[45.518751, -122.678794], zoom_start=15)
 
+# List of coordinates
+coords = [[45.518751, -122.678794], [45.520426, -122.677938],[45.521335, -122.681368], [45.518020, -122.683182], [45.517104, -122.679727]]
 # Create feature group and add to map
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[45.518751, -122.678794], popup="Hi I am a marker", icon=folium.Icon(color='green')))
+
+
+for coordinates in coords:
+	fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a marker", icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 # Save map
