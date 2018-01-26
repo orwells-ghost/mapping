271345b7de5a9cc4666b850d6c20ed6a4d7eb475
Ryan Kennedy <33361057+orwells-ghost@users.noreply.github.com>
2018-01-26 00:19:56 -0800

    Create pandas dataframe and generated map

--- generate_map.py
+++ generate_map.py
@@ -7,13 +7,17 @@ map = folium.Map(location=[39.8283, -98.5795], zoom_start=6)
 # Create feature group and add to map
 fg = folium.FeatureGroup(name="Pandas Map")
 
+# Read data from csv and save to dataframe
 data = pandas.read_csv("us_volcano_data.csv")
 lat = list(data["Latitude"])
 lon = list(data["Longitude"])
+elev = list(data["Elevation"])
 
-for lt, ln in zip(lat, lon):
-	fg.add_child(folium.Marker(location=[lt, ln], popup="Hi, are you lost?", icon=folium.Icon(color='green')))
+# Loop through each list simultaneously
+for lt, ln, el in zip(lat, lon, elev):
+	fg.add_child(folium.Marker(location=[lt, ln], popup=str(el), icon=folium.Icon(color='green')))
 
+# Add child fg (Marker) to map
 map.add_child(fg)
 
 # Save map
