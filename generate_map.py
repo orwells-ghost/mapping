1ecf5e5f1cfd3e9513fa2d7e00874e6d390e6c94
Ryan Kennedy <33361057+orwells-ghost@users.noreply.github.com>
2018-01-30 21:52:00 -0800

    Added polygon layer based on color/country

--- generate_map.py
+++ generate_map.py
@@ -33,11 +33,16 @@ map = folium.Map(location=[39.8283, -98.5795], zoom_start=6)
 # Create feature group and add to map
 fg = folium.FeatureGroup(name="Pandas Map")
 
-# Loop through each list simultaneously
-for lt, ln, el, vn in zip(lat, lon, elev, vName):
-	string = vn + ": " + str(el) + " feet"
-	fg.add_child(folium.CircleMarker(location=[lt, ln], popup=str(string), fill_color=color_producer(el), 
-		color='grey', fill_opacity=0.7, radius=2500))
+# # Loop through each list simultaneously
+# for lt, ln, el, vn in zip(lat, lon, elev, vName):
+# 	string = vn + ": " + str(el) + " feet"
+# 	fg.add_child(folium.CircleMarker(location=[lt, ln], popup=str(string), fill_color=color_producer(el), 
+# 		color='grey', fill_opacity=0.7, radius=2500))
+
+# Load json file with world geo data
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'), 
+	style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+	else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 # Add child fg (Marker) to
 map.add_child(fg)
